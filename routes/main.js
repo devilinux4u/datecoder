1897a2ea1386e8ce08eb44af540b53cb76b4681e
sololinux <chauraj345r@gmail.com>
2024-12-18 00:54:02 +0545

    vercel 1st push

--- routes/main.js
+++ routes/main.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const { user } = require('../db/db');
+const { user, match } = require('../db/db');
 const { isUser } = require('../middleware/session')
 
 
 router.get('/main', isUser, async (req, res) => {
     try {
         const cId = req.session.isUser.id;
-        const cGen = (await user.findOne({ log_id: cId }, 'gender').lean()).gender;
-        const oGen = cGen === 'male' ? 'female' : 'male';
+        const cUser = await user.findOne({ log_id: cId }).lean();
+        const oGen = cUser.gender === 'male' ? 'female' : 'male';
+
+        const userMatches = await match.findOne({ log_id: cId });
+
+        let filterCondition = {};
+        if (userMatches && userMatches.like.length > 0) {
+            filterCondition = { log_id: { $nin: userMatches.like } };
+        }
 
         const profiles = await user.find({
-            gender: oGen
+            gender: oGen,
+            ...filterCondition
         }).lean();
+
 
         const suff = profiles.sort(() => Math.random() - 0.5);
 
-        res.render('main', { profiles: suff, check: false });
+        let lastMatchedUser = null;
+        if (userMatches && userMatches.match.length > 0) {
+            const lastMatchId = userMatches.match[Math.floor(Math.random() * userMatches.match.length)];
+            lastMatchedUser = await user.findOne({ log_id: lastMatchId }).lean();
+        }
+
+        res.render('main', { profiles: suff, check: lastMatchedUser, uimg: cUser.photo, num: userMatches });
     }
     catch (err) {
         console.error('Error fetching profiles:\n', err);
