c14d92487a5087a48d2df71e91db5e51c4c415eb
Kamal Basnet <101182521+devilinux4u@users.noreply.github.com>
2026-01-06 16:42:40 +0545

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,17 +2,10 @@ require('dotenv').config({ path: '../.env' });
 const mongoose = require("mongoose");
 const models = require('./model'); 
 
-const user = encodeURIComponent('sololinux');
-const pass = encodeURIComponent(process.env.pass);
-const cluster = 'sololinux.errs8e9.mongodb.net';
-
-const uri = `mongodb+srv://${user}:${pass}@${cluster}/?appName=sololinux`;
-
 try {
     mongoose
         .connect(process.env.MONGODB_URI)
         .then(() => console.log(`MongoDB connected Successfully..!`));
-}
 catch (error) {
     console.log(`MongoDB Error: `, error.message);
     process.exit(1);
